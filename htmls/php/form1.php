a9311352b8aaf5bb599bafb64cfd928425facdb0
Alkaponees <mr.sergey1898@gmail.com>
2023-04-06 22:25:42 +0300

    Modify form1.php and form1.html

--- htmls/php/form1.php
+++ htmls/php/form1.php
@@ -12,7 +12,16 @@ $conn= new mysqli($hostname,$username,$password,$dbname);
  if(!$conn){
     die ("Connection failed: "). mysqli_connect_error();
  }
- echo "Connected successfully"; 
+echo "Connected successfully";
+
+$user_name=$_POST["user_name"];
+$phone=$_POST["user_name"];
+$hobby=$_POST["user_name"];
+$university=$_POST["user_name"];
+$course=$_POST["user_name"];
+
+
+
 $query1 = "INSERT INTO $usertable VALUES
  ('$user_name','$phone','$hobby','$university','$course')";
 $result1 = MYSQLI_QUERY($conn,$query1);
